d00ed60b24ed4d63962a14f5fb2ba62b257f57b5
Eskil Opdahl Nordland <eskil.nordland@gmail.com>
2017-06-01 23:09:36 +0200

    added more tests

---
+++ __tests__/controller.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const path = require('path');
+const assert = require('yeoman-assert');
+const helpers = require('yeoman-test');
+const test = require('ava');
+
+test.before(() => {
+  return helpers.run(path.join(__dirname, '../generators/controller'))
+    .withPrompts({
+      controller: 'coo_controller',
+      mode: 'Coo',
+      route: '/coo'
+    });
+});
+
+test('swagger-es-6:controller ', () => {
+  assert.file([
+    'src/api/controllers/coo_controller.js',
+    'src/test/controllers/coo_controller.js'
+  ]);
+});
---
+++ __tests__/model.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const path = require('path');
+const assert = require('yeoman-assert');
+const helpers = require('yeoman-test');
+const test = require('ava');
+
+test.before(() => {
+  return helpers.run(path.join(__dirname, '../generators/model'))
+    .withPrompts({
+      name: 'Coo'
+    });
+});
+
+test('swagger-es-6:model ', () => {
+  assert.file([
+    'src/models/Coo.js'
+  ]);
+});
